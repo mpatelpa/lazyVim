c7d0763831fa09f5f9725ffcbf579973e830e6cc
mpatelpa <mpatelpa@mpatelpa-lx.local>
2024-03-26 18:06:49 -0400

    Updated files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+-- Yank into system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", '"*y') -- yank motion
+--vim.keymap.set({ "n", "v" }, "<leader>Y", '"*Y') -- yank line
+--
